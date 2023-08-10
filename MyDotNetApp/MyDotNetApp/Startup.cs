0ebc980cbbedb2def69faa600d8a13bf6847f944
Ikeobi-ctu <20231306@ctucareer.co.za>
2023-08-10 09:27:23 +0200

    add YAML file

---
+++ MyDotNetApp/MyDotNetApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyDotNetApp.Startup))]
+namespace MyDotNetApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
